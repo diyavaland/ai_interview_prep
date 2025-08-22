b7a34a43b5ad88dcaebcd7085769e878c549a35d
diyavaland <151962108+diyavaland@users.noreply.github.com>
2025-08-22 17:43:47 -0600

    first commit

---
+++ (auth)/sign-in/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import AuthForm from "@/components/AuthForm"; // ✅ adjust path if needed
+
+const Page = () => {
+    return <AuthForm  type = "sign-in"/>;
+};
+
+export default Page;
