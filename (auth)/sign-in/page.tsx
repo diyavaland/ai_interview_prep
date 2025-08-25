6be3a2a2fd9c4cc3f684e999058825622402c6ca
diyavaland <151962108+diyavaland@users.noreply.github.com>
2025-08-25 15:54:01 -0600

    Routes fixed

--- (auth)/sign-in/page.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import AuthForm from "@/components/AuthForm"; // ✅ adjust path if needed
-
-const Page = () => {
-    return <AuthForm  type = "sign-in"/>;
-};
-
-export default Page;
